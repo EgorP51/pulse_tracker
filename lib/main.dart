ab177c722432016f4f93e1b7f2757221f8e142a1
Yehor Pustovoit <63584ee@gmail.com>
2024-02-26 15:11:47 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PulseTrackerApp());
+}
+
+class PulseTrackerApp extends StatelessWidget {
+  const PulseTrackerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
